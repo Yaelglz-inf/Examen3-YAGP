05b38026aadedd86092ac8eac3df5981442b28c8
Yaelglz-inf <nvgs502@gmail.com>
2026-06-10 21:03:14 -0600

    Validación de credenciales

--- app/src/main/java/com/example/examen3/MainActivity.kt
+++ app/src/main/java/com/example/examen3/MainActivity.kt
@@ -1,20 +1,48 @@
 package com.example.examen3
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.examen3.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val dao = MyApplication.getDatabase(this).usuarioDao()
+
+        binding.btnLogin.setOnClickListener {
+            val nombre   = binding.etNombre.text.toString().trim()
+            val password = binding.etPassword.text.toString().trim()
+
+            val usuario = dao.validarCredenciales(nombre, password)
+            if (usuario != null) {
+                val intent = Intent(this, ProfileActivity::class.java)
+                intent.putExtra("NOMBRE", nombre)
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, "Credenciales incorrectas", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        binding.btnRegister.setOnClickListener {
+            startActivity(Intent(this, SignUpActivity::class.java))
         }
     }
 }
